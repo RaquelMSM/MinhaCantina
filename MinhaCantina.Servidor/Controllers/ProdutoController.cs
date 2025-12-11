7434c1b5c5c60093c49e36d6d54772d6a44ea40d
Gaaldra <55445484+Gaaldra@users.noreply.github.com>
2025-12-11 16:59:11 -0300

    Cria a rota para deletar os produtos

--- MinhaCantina.Servidor/Controllers/ProdutoController.cs
+++ MinhaCantina.Servidor/Controllers/ProdutoController.cs
@@ -66,7 +66,7 @@ public class ProdutoController(MinhaCantinaContexto cantinaContexto) : Controlle
 		return StatusCode(200, produtos);
 	}
 
-	// Rota: site.com/Produto/alterar_nome/{}
+	// Rota: localhost:7207/Produto/alterar_nome
 	[HttpPatch("alterar_nome")]
 	public IActionResult AlterarNomeProduto(int produtoId, string novoNome)
 	{
@@ -83,7 +83,7 @@ public class ProdutoController(MinhaCantinaContexto cantinaContexto) : Controlle
 			_contexto.Produtos.Update(produto);
 			_contexto.SaveChanges();
 		}
-		catch(DbUpdateException excecao)
+		catch (DbUpdateException excecao)
 		{
 			var excecaoInterna = excecao.InnerException;
 
@@ -101,6 +101,67 @@ public class ProdutoController(MinhaCantinaContexto cantinaContexto) : Controlle
 		{
 			return StatusCode(500, $"Ocorreu um erro inesperado: {excecao.Message}");
 		}
+
+		return StatusCode(204);
+	}
+
+	// Rota: localhost:7207/Produto/alterar_preco
+	[HttpPatch("alterar_preco")]
+	public IActionResult AlterarPrecoProduto(int produtoId, decimal novoPreco)
+	{
+		// Buscar o produto
+		Produto? produto = _contexto.Produtos.Find(produtoId);
+
+		if (produto is null) return StatusCode(400, "O produto procurado não existe");
+
+		try
+		{
+			produto.MudarPreco(novoPreco);
+			_contexto.Produtos.Update(produto);
+			_contexto.SaveChanges();
+		}
+		catch (Exception excecao)
+		{
+			return StatusCode(500, $"Ocorreu um erro inesperado: {excecao.Message}");
+		}
+
+		return StatusCode(204);
+	}
+
+	// Rota: localhost:7207/Produto/alterar_categoria
+	[HttpPatch("alterar_categoria")]
+	public IActionResult AlterarProdutoCategoria(int produtoId, int novaCategoriaId)
+	{
+		Produto? produto = _contexto.Produtos.Find(produtoId); // Procurar o produto
+		if (produto is null) return StatusCode(400, "Este produto não existe"); // Retorna para o cliente erro bad request se produto não existe
+
+		Categoria? categoria = _contexto.Categorias.Find(novaCategoriaId); // Procurar a categoria nova
+		if (categoria is null) return StatusCode(400, "Esta categoria não existe");// Retorna para o cliente erro bad request se categoria não existe
+
+		produto.MudarCategoria(categoria);
+
+		try
+		{
+			_contexto.Produtos.Update(produto);
+			_contexto.SaveChanges();
+		}
+		catch (Exception excecao)
+		{
+			return StatusCode(500, $"Ocorreu um erro inesperado: {excecao.Message}");
+		}
+
+		return StatusCode(204);
+	}
+
+	// Rota: localhost:7207/Produto/deletar_produto/{produtoId}
+	[HttpDelete("deletar_produto/{produtoId}")]
+	public IActionResult DeletarProduto(int produtoId)
+	{
+		Produto? produtoObjeto = _contexto.Produtos.Find(produtoId);
+		if (produtoObjeto is null) return StatusCode(400, "Este produto não existe");
+
+		_contexto.Produtos.Remove(produtoObjeto);
+		_contexto.SaveChanges();
 
 		return StatusCode(204);
 	}
